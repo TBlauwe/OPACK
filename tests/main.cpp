3a9d307b024dea6d70e6efae8de753980cb54723
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2022-04-02 18:23:27 +0200

    Fixed docs target Added tests placeholder

---
+++ tests/main.cpp
@@ -0,0 +1,2 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN // Do not delete ! Provide a main so we can run tests.
+#include <doctest/doctest.h>
---
+++ tests/simulation.cpp
@@ -0,0 +1,11 @@
+#include <doctest/doctest.h>
+#include <opack/core/simulation.hpp>
+
+TEST_SUITE_BEGIN("Simulation");
+
+TEST_CASE("Basics") 
+{
+	CHECK(true);
+}
+
+TEST_SUITE_END();
