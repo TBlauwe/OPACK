bba876718f245634bd7a0aafe31191f59012fba1
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2022-09-19 17:16:53 +0200

    Experiment X01 : now generating color sequences to visualize behaviour.

--- experiments/01_sensibility/01_sensibility.cpp
+++ experiments/01_sensibility/01_sensibility.cpp
@@ -6,12 +6,29 @@
 #include <opack/module/simple_agent.hpp>
 #include <matplot/matplot.h>
 #include <color/color.hpp>
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+template<typename T>
+double percentage(const T& min, const T& max, const T& value)
+{
+    return (value - min) / (max - min);
+}
+
+uint8_t random_color()
+{
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
+    static std::uniform_int_distribution<std::mt19937::result_type> dist(0,255); // distribution in range [1, 6]
+    return dist(rng);
+}
 
 struct Color
 {
-    uint8_t r{ 0 };
-    uint8_t g{ 0 };
-    uint8_t b{ 0 };
+    uint8_t r{random_color()};
+    uint8_t g{random_color()};
+    uint8_t b{random_color()};
 };
 
 struct Memory
@@ -25,6 +42,46 @@ struct ActionSelection : opack::operations::SelectionByIGraph<SuitableActions> {
 struct Act : opack::operations::All<opack::df<ActionSelection, opack::Action_t>> {};
 
 using namespace matplot;
+
+void generate_color_sequences(opack::World& world)
+{
+	
+}
+
+void generate_actions_sequence(opack::World& world)
+{
+	std::tuple<vector_2d, vector_2d, vector_2d> C;
+	auto &[r, g, b] = C;
+
+	int max_size = 0;
+	std::vector<std::string> tick_labels{};
+	world.filter<const Memory>().each([&r, &g, &b, &max_size, &tick_labels](opack::Entity agent, const Memory& memory)
+		{
+			auto sub_r = vector_1d{};
+			auto sub_g = vector_1d{};
+			auto sub_b = vector_1d{};
+			fmt::print("Agent has done : \n");
+			for(auto& color : memory.actions_done)
+			{
+				fmt::print(" - ({},{},{})\n", color.r, color.g, color.b);
+				sub_r.push_back(color.r);
+				sub_g.push_back(color.g);
+				sub_b.push_back(color.b);
+			}
+			r.push_back(sub_r);
+			g.push_back(sub_g);
+			b.push_back(sub_b);
+			tick_labels.push_back(agent.path().c_str());
+		}
+	);
+	image(C);
+
+	std::vector<double> ticks(opack::count_instance<simple::Agent>(world));
+	std::iota(ticks.begin(), ticks.end(),1);
+	yticks(ticks);
+	yticklabels(tick_labels);
+	show();
+}
 
 int main()
 {
@@ -47,8 +104,14 @@ int main()
     opack::prefab<simple::Agent>(world).add<Memory>().add<simple::Flow>();
 
     // Instantiate "Action" prefab
-    opack::init<Action>(world).require<simple::Actuator>().add<Color>();
-    opack::on_action_begin<Action>(world, [](opack::Entity action) {std::cout << action << " is being done\n"; });
+    opack::init<Action>(world).require<simple::Actuator>().override<Color>();
+    opack::on_action_begin<Action>(world, [](opack::Entity action)
+    {
+            auto agent = opack::initiator(action);
+            if (agent.has<Memory>())
+                agent.get_mut<Memory>()->actions_done.push_back(*action.get<Color>());
+    		std::cout << action << " is being done\n";
+    });
 
     world.plecs_from_file("configuration.flecs");
 
@@ -60,6 +123,7 @@ int main()
         [](opack::Entity e, auto& inputs)
 			{
         		SuitableActions::iterator(inputs) = opack::action<Action>(e);
+
                 return opack::make_outputs<SuitableActions>();	
 			}
         );
@@ -92,12 +156,11 @@ int main()
     // World population
     // =========================================================================== 
 	opack::spawn<simple::Agent>(world);
+	opack::spawn<simple::Agent>(world);
 
-    std::vector<std::vector<double>> C = {
-        {0, 2, 4, 6}, {8, 10, 12, 14}, {16, 18, 20, 22}};
-    image(C, false);
-    colorbar();
-	opack::run_with_webapp(world);
-    //opack::step_n(world, 10);
+    opack::step_n(world, 10);
+	//opack::run_with_webapp(world);
+
+	generate_actions_sequence(world);
     return 0;
 }
