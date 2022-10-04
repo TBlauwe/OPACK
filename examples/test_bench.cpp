9ad04ff1cc99353dd68aaa88996b6422838a7f7e
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2022-10-04 17:11:07 +0200

    Experimenting a way to get value from string

--- examples/test_bench.cpp
+++ examples/test_bench.cpp
@@ -1,29 +1,38 @@
 #include <flecs.h>
-#include <fmt/core.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
 
-enum class Status
+struct Message
 {
-	FREE,
-    TAKEN
+    const char* value;
 };
 
-struct R{};
-
-inline void print(flecs::entity_view entity)
+struct AnotherMessage
 {
-	fmt::print("Entity path {}\n", entity.path());
-}
+    const char* value;
+};
+
+using json = nlohmann::json;
 
 int main(int, char* [])
 {
     flecs::world ecs;
-    ecs.component<Status>()
-        .constant("FREE", static_cast<int32_t>(Status::FREE))
-        .constant("TAKEN", static_cast<int32_t>(Status::TAKEN))
-        ;
-    auto prefab = ecs.prefab("TestEntity").add(Status::FREE);
-    auto entity = ecs.entity("TestEntity2").is_a(prefab);
-    fmt::print("{}", entity.has(Status::FREE));
-    ecs.system().term(Status::FREE).each(print);
+    ecs.component<Message>()
+		.member(flecs::String, "value");
+
+    ecs.component<AnotherMessage>()
+		.member(flecs::String, "value");
+
+    ecs.plecs_from_str("test", "test_entity { - Message{\"Hello world !\"} - AnotherMessage{\"Super Hello world !\"}} ");
+    auto entity = ecs.lookup("test_entity");
+	flecs::entity_to_json_desc_t desc;
+    desc.serialize_path = true;
+    desc.serialize_label = false;
+    desc.serialize_values = true;
+    desc.serialize_type_info = false;
+    auto value = json::parse(std::string(entity.to_json(&desc)));
+    value["values"]["value"];
+    std::cout <<  value.dump() << "\n"; // Prints : 
+    std::cout << entity.get<Message>()->value << "\n"; // Prints : 
     ecs.app().enable_rest().run();
 }
