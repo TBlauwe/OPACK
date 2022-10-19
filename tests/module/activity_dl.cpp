8041dca0ca49f997f75fe9e1e66555407486e900
Tristan de Blauwe <tristan.de-blauwe@etu.utc.fr>
2022-10-19 16:11:11 +0200

    Refactoring adl test suite (please save me)

--- tests/module/activity_dl.cpp
+++ tests/module/activity_dl.cpp
@@ -5,6 +5,8 @@
 #include <opack/module/flows.hpp>
 #include <opack/module/simple_agent.hpp>
 
+struct Satisfied {};
+
 TEST_CASE("API Activity-DL")
 {
 	MESSAGE("SETUP ...");
@@ -15,7 +17,6 @@ TEST_CASE("API Activity-DL")
 	ADL_ACTIVITY(Activity_A);
 	ADL_ACTIVITY(Activity_B);
 	struct Data { float value{ 0.0f }; };
-	struct Satisfied {};
 	struct ReadyA {};
 	struct ReadyB {};
 
@@ -150,7 +151,7 @@ TEST_CASE("API Activity-DL")
 			CHECK(actions.size() == 0);
 		}
 
-		opack::step(world, 0.1f);
+		opack::step_n(world, 2, 0.1f);
 		MESSAGE("Failed activity"); // Since task is finished but not satisfied
 		{
 			actions.clear();
@@ -179,172 +180,166 @@ TEST_CASE("API Activity-DL")
 	}
 }
 
+void begin(flecs::entity e) { e.add<opack::Cycle::Begin, opack::Timestamp>();}
+void end(flecs::entity e) { e.add<opack::Cycle::End, opack::Timestamp>();}
+void satisfy(flecs::entity e) { e.add<Satisfied>();}
 
-//TEST_CASE("API Activity-DL")
-//{
-//	OPACK_AGENT(MyAgent);
-//	ADL_ACTIVITY(Activity_A);
-//	ADL_ACTIVITY(Activity_B);
-//	struct ReadyA {};
-//	struct ReadyB {};
-//	struct Satisfied {};
-//	struct Data { float value{ 1.0 }; };
-//
-//	OPACK_ACTION(Action1);
-//	OPACK_ACTION(Action2);
-//	OPACK_ACTION(Action3);
-//
-//	auto world = opack::create_world();
-//	world.import<adl>();
-//
-//	opack::init<Action1>(world);
-//	opack::init<Action2>(world);
-//	opack::init<Action3>(world);
-//	opack::init<MyAgent>(world);
-//
-//	auto agent = opack::spawn<MyAgent>(world);
-//
-//	std::vector<flecs::entity> actions{};
-//
-//	auto root = adl::activity<Activity_A>(world);
-//	auto action = adl::action<Action1>(root);
-//	SUBCASE("Simple tree")
-//	{
-//		adl::condition<adl::Satisfaction>(root, adl::is_finished);
-//		adl::condition<adl::Satisfaction>(action, adl::is_finished);
-//
-//		CHECK(root.get<adl::Constructor>()->logical == adl::LogicalConstructor::AND);
-//		CHECK(root.get<adl::Constructor>()->temporal == adl::TemporalConstructor::SEQ_ORD);
-//		CHECK(adl::has_children(root));
-//		CHECK(adl::children_count(root) == 1);
-//		CHECK(adl::size(root) == 2);
-//		CHECK(adl::is_root(root));
-//		CHECK(!adl::is_root(action));
-//
-//		auto instance = opack::spawn<Activity_A>(world);
-//		CHECK(instance.get<adl::Constructor>()->logical == adl::LogicalConstructor::AND);
-//		CHECK(instance.get<adl::Constructor>()->temporal == adl::TemporalConstructor::SEQ_ORD);
-//		CHECK(adl::is_root(instance));
-//		CHECK(adl::children_count(instance) == 1);
-//		CHECK(adl::size(instance) == 2);
-//		CHECK(!adl::is_satisfied(instance));
-//	}
-//
-//	SUBCASE("Context")
-//	{
-//		auto instance = opack::spawn<Activity_A>(world);
-//		adl::ctx_entity<opack::Agent>(instance, agent);
-//		adl::ctx_value<Data>(instance, { 3.0 });
-//		instance.children([&](flecs::entity child)
-//			{
-//				CHECK(adl::ctx_entity<opack::Agent>(child) == agent);
-//				CHECK(adl::ctx_value<Data>(child)->value == 3.0);
-//			});
-//	}
-//
-//	SUBCASE("Composition")
-//	{
-//		auto activity_b = adl::activity<Activity_B>(world);
-//		auto instance = adl::compose<Activity_A>(activity_b);
-//		CHECK(adl::size(instance) == 2);
-//		CHECK(adl::size(activity_b) == 3);
-//	}
-//
-//	SUBCASE("Contextual Condition")
-//	{
-//		adl::condition<adl::Contextual>(root, [](opack::EntityView task) { return adl::ctx_entity<opack::By>(task).has<ReadyA>(); });
-//		adl::condition<adl::Contextual>(action, [](opack::EntityView task) { return adl::ctx_entity<opack::By>(task).has<ReadyB>(); });
-//		adl::condition<adl::Satisfaction>(action, [](opack::EntityView task) { return task.has<Satisfied>(); });
-//		auto instance = opack::spawn<Activity_A>(world);
-//		adl::ctx_entity<opack::By>(instance, agent);
-//
-//		CHECK(instance.has<adl::Contextual>());
-//		instance.children([](opack::Entity e)
-//			{
-//				CHECK(e.has<adl::Contextual>());
-//				CHECK(e.has<adl::Satisfaction>());
-//			});
-//
-//		MESSAGE("0 possible actions because agent is not ready A yet");
-//		{
-//			auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//			CHECK(actions.size() == 0);
-//			CHECK(!success);
-//			CHECK(!adl::in_progress(instance));
-//		}
-//
-//		agent.add<ReadyA>();
-//		MESSAGE("0 possible actions because agent is not ready B yet");
-//		{
-//			auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//			CHECK(actions.size() == 0);
-//			CHECK(!success);
-//			CHECK(!adl::in_progress(instance));
-//		}
-//
-//		agent.add<ReadyB>();
-//		MESSAGE("1 possible actions since agent is ready");
-//		{
-//			auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//			CHECK(actions.size() == 1);
-//			CHECK(!success);
-//			CHECK(!adl::in_progress(instance));
-//		}
-//
-//		MESSAGE("Check success");
-//		{
-//			MESSAGE("Fail because there are still potential actions."); // Since no task have been finished successfuly
-//			{
-//				actions.clear();
-//				auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//				CHECK(actions.size() == 1);
-//				CHECK(!success);
-//				CHECK(!adl::in_progress(instance));
-//			}
-//
-//			auto a = actions.at(0);
-//			a.set<opack::Begin, opack::Timestamp>({ 0.0f });
-//			MESSAGE("Fail because there are still actions in progress."); // Since task is not finished
-//			{
-//				actions.clear();
-//				auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//				CHECK(actions.size() == 0);
-//				CHECK(!success);
-//				CHECK(adl::in_progress(instance));
-//			}
-//
-//			a.set<opack::End, opack::Timestamp>({ 0.0f });
-//			MESSAGE("Failed activity"); // Since task is finished but not satisfied
-//			{
-//				actions.clear();
-//				auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//				CHECK(actions.size() == 0);
-//				CHECK(!success);
-//				CHECK(!adl::in_progress(instance));
-//			}
-//
-//			a.add<Satisfied>();
-//			MESSAGE("Succeeded activity"); // Since task is finished  and satisfied
-//			{
-//				actions.clear();
-//				auto success = adl::compute_potential_actions(instance, std::back_inserter(actions));
-//				CHECK(actions.size() == 0);
-//				CHECK(success);
-//				CHECK(!adl::in_progress(instance));
-//			}
-//		}
-//	}
-//
-//	SUBCASE("Traversal")
-//	{
-//		auto instance = opack::spawn<Activity_A>(world);
-//
-//		int counter{ 0 };
-//		adl::traverse_dfs(instance, [&counter](flecs::entity task) { counter++; });
-//		CHECK(counter == 2);
-//	}
-//}
-//
+
+struct step
+{
+	std::string title {};	
+	std::vector<flecs::entity>	expected_actions {};	
+	bool in_progress {false};	
+	bool is_satisfied {false};	
+	bool is_finished {false};	
+
+	void check(flecs::entity task)
+	{
+		MESSAGE("Step : ", title);
+		std::vector<flecs::entity> actions;
+		adl::compute_potential_actions(task, std::back_inserter(actions));
+
+		CHECK(actions == expected_actions);
+		CHECK(adl::in_progress(task) == in_progress);
+		CHECK(adl::is_satisfied(task) == is_satisfied);
+		CHECK(adl::is_finished(task) == is_finished);
+	}
+};
+
+TEST_CASE("Activity-DL operators")
+{
+	MESSAGE("SETUP ...");
+	OPACK_FLOW(MyFlow);
+	OPACK_SUB_ACTION(Action_1, adl::Action);
+	OPACK_SUB_ACTION(Action_2, adl::Action);
+	OPACK_SUB_ACTION(Action_3, adl::Action);
+	ADL_ACTIVITY(Activity_A);
+
+	auto world = opack::create_world();
+	opack::import<simple>(world);
+	opack::import<adl>(world);
+
+	opack::prefab<simple::Agent>(world).add<MyFlow>();
+
+	opack::init<Action_1>(world).duration(1.0f).require<simple::Actuator>();
+	opack::init<Action_2>(world).duration(1.0f).require<simple::Actuator>();
+	opack::init<Action_3>(world).duration(1.0f).require<simple::Actuator>();
+
+	ActivityFlowBuilder<MyFlow, adl::Activity>(world).build();
+	auto agent = opack::spawn<simple::Agent>(world);
+
+	MESSAGE("SETUP ... done");
+
+	auto root = adl::activity<Activity_A>(world);
+	auto action1 = adl::action<Action_1>(root);
+	auto action2 = adl::action<Action_2>(root);
+	auto action3 = adl::action<Action_3>(root);
+	auto lambda = [](opack::EntityView task) {return task.has<Satisfied>(); };
+	adl::condition<adl::Satisfaction>(action1, lambda);
+	adl::condition<adl::Satisfaction>(action2, lambda);
+	adl::condition<adl::Satisfaction>(action3, lambda);
+
+	MESSAGE("IND OR");
+	{
+		std::vector<flecs::entity> actions{};
+		auto instance = opack::spawn<Activity_A>(world);
+		instance.set<adl::Constructor>({ adl::LogicalConstructor::OR, adl::TemporalConstructor::IND });
+		auto children = adl::children(instance);
+		auto a1 = children.at(1);
+		auto a2 = children.at(2);
+		auto a3 = children.at(3);
+
+		step{
+			.title = "START",
+			.expected_actions {a1, a2, a3},
+			.in_progress = false,
+			.is_satisfied = false,
+			.is_finished = false
+		}.check(instance);
+
+		begin(a1);
+		step{
+			.title = "Begin a1",
+			.expected_actions {a2, a3},
+			.in_progress = true,
+			.is_satisfied = false,
+			.is_finished = false
+		}.check(instance);
+
+		begin(a2);
+		step{
+			.title = "Begin a2",
+			.expected_actions {a3},
+			.in_progress = true,
+			.is_satisfied = false,
+			.is_finished = false
+		}.check(instance);
+
+		end(a1);
+		step{
+			.title = "End a1",
+			.expected_actions {a3},
+			.in_progress = true,
+			.is_satisfied = false,
+			.is_finished = false
+		}.check(instance);
+
+		end(a2);
+		step{
+			.title = "End a2",
+			.expected_actions {a3},
+			.in_progress = false,
+			.is_satisfied = false,
+			.is_finished = false
+		}.check(instance);
+
+		satisfy(a2);
+		step{
+			.title = "Satisfy a2",
+			.expected_actions {a3},
+			.in_progress = false,
+			.is_satisfied = true,
+			.is_finished = false
+		}.check(instance);
+
+		begin(a3);
+		step{
+			.title = "Begin a3",
+			.expected_actions {},
+			.in_progress = true,
+			.is_satisfied = true,
+			.is_finished = false
+		}.check(instance);
+
+		end(a3);
+		step{
+			.title = "End a3",
+			.expected_actions {},
+			.in_progress = false,
+			.is_satisfied = true,
+			.is_finished = false
+		}.check(instance);
+
+		satisfy(a1);
+		step{
+			.title = "Satisfy a1",
+			.expected_actions {},
+			.in_progress = false,
+			.is_satisfied = true,
+			.is_finished = false
+		}.check(instance);
+
+		satisfy(a2);
+		step{
+			.title = "Satisfy a2",
+			.expected_actions {},
+			.in_progress = false,
+			.is_satisfied = true,
+			.is_finished = false
+		}.check(instance);
+	}
+}
+
 //TEST_CASE("API Activity-DL - Operators")
 //{
 //	OPACK_AGENT(MyAgent);
